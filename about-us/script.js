62816240907fa87ee1d93574a254e1fbae23f281
sofia lopez <lsmunoz@unimayor.edu.co>
2025-11-22 12:47:21 -0500

    añadir about us a la web

---
+++ about-us/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnMessage").addEventListener("click", function() {
+    const message = document.getElementById("specialMessage");
+    message.classList.toggle("hidden");
+});
